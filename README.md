4c4244e9d165e12836d7aad14b0968646c902d44
Matthew Ryall <matthew@mexico86.co.uk>
2019-06-19 11:46:59 +0100

    Factor initial code and add error handling

--- README.md
+++ README.md
@@ -7,6 +7,17 @@ docker run --rm -v "$PWD":/var/task \
     -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY \
     -e AWS_SESSION_TOKEN -e AWS_SECURITY_TOKEN \
     -e AWS_REGION=eu-west-1 \
-    lambci/lambda:go1.x aws-ssm-access "{ \"parameterName\": \"AParameterName\" }"
+    lambci/lambda:go1.x aws-ssm-parameter-access \
+    "{
+        \"RequestType\" : \"Create\",
+        \"ResponseURL\" : \"https://presignedS3URL/\",
+        \"StackId\" : \"arn:aws:cloudformation:us-west-2:123456789012:stack/stack-name/guid\",
+        \"RequestId\" : \"unique id for this create request\",
+        \"ResourceType\" : \"Custom::TestResource\",
+        \"LogicalResourceId\" : \"MyTestResource\",
+        \"ResourceProperties\" : {
+            \"ParameterName\" : \"ATestParameter\"
+        }
+    }"
 
 ```
--- main.go
+++ main.go
@@ -2,21 +2,113 @@ package main
 
 import (
 	"./ssmaccess"
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"net/http"
 )
 
-type event struct {
-	ParameterName string `json:"parameterName"`
+// CloudFormationCustomResourceEvent -- an event
+type CloudFormationCustomResourceEvent struct {
+	RequestType        string `json:"RequestType"`
+	ResponseURL        string `json:"ResponseURL"`
+	StackID            string `json:"StackId"`
+	RequestID          string `json:"RequestId"`
+	ResourceType       string `json:"ResourceType"`
+	LogicalResourceID  string `json:"LogicalResourceId"`
+	ResourceProperties struct {
+		ParameterName string `json:"ParameterName"`
+	} `json:"ResourceProperties"`
 }
 
-func handleRequest(ctx context.Context, event event) (string, error) {
-	p, err := ssmaccess.SSMParameterAccess(event.ParameterName)
+// CloudFormationCustomResourceResponse -- a response
+type CloudFormationCustomResourceResponse struct {
+	Status             string `json:"Status"`
+	NoEcho             bool   `json:"NoEcho"`
+	StackID            string `json:"StackId"`
+	RequestID          string `json:"RequestId"`
+	LogicalResourceID  string `json:"LogicalResourceId"`
+	PhysicalResourceID string `json:"PhysicalResourceId"`
+	Data               struct {
+		ParameterValue string `json:"ParameterValue"`
+	} `json:"Data"`
+}
+
+// handle the CloudFormation request
+func handleRequest(ctx context.Context, inputEvent json.RawMessage) error {
+
+	// Unmarshall here rather than on invocation to ensure we catch errors
+	var event CloudFormationCustomResourceEvent
+	err := json.Unmarshal(inputEvent, &event)
 	if err != nil {
-		return "", err
+		return fmt.Errorf("Input event error: %v", err)
 	}
 
-	return p, nil
+	// Grab the named parameter value from SSM
+	p, err := ssmaccess.SSMParameterAccess(event.ResourceProperties.ParameterName)
+	if err != nil {
+		return fmt.Errorf("SSM access error: %v", err)
+	}
+
+	// Build a response struct for CloudFormation
+	res := buildCloudFormationResponse(event, p)
+	if err != nil {
+		return fmt.Errorf("CloudFormation response error: %v", err)
+	}
+
+	// Convert the CloudFormation response to JSON
+	JSON, err := BuildJSON(&res)
+	if err != nil {
+		return fmt.Errorf("JSON error: %v", err)
+	}
+
+	// Create an HTTP client to send the S3 request to the presigned URL
+	client := &http.Client{}
+	req, err := http.NewRequest("PUT", event.ResponseURL, bytes.NewReader(JSON))
+	if err != nil {
+		return fmt.Errorf("http request error: %v", err)
+	}
+
+	// Send the HTTP request to write out the response data to S3
+	req.ContentLength = int64(len(JSON))
+	s3res, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("S3 request error: %v", err)
+	}
+	if s3res.StatusCode >= http.StatusBadRequest {
+		return fmt.Errorf("S3 response error: %v", s3res)
+	}
+
+	return nil
+}
+
+func buildCloudFormationResponse(event CloudFormationCustomResourceEvent, parameterValue string) CloudFormationCustomResourceResponse {
+	// Create a response struct
+	res := CloudFormationCustomResourceResponse{
+		Status:             "SUCCESS",
+		NoEcho:             true,
+		StackID:            event.StackID,
+		RequestID:          event.RequestID,
+		LogicalResourceID:  event.LogicalResourceID,
+		PhysicalResourceID: event.ResourceProperties.ParameterName,
+	}
+
+	// Add the value here as it's a nested struct
+	res.Data.ParameterValue = parameterValue
+
+	return res
+}
+
+// BuildJSON builds a JSON string from a CF Response
+func BuildJSON(res *CloudFormationCustomResourceResponse) ([]byte, error) {
+	resJSON, err := json.Marshal(res)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return resJSON, nil
 }
 
 func main() {
